d63662841b93c98fdfc38243c2758b25b2be2273
Edycar Reyes <edycarreyes@gmail.com>
2020-02-07 17:42:47 -0600

    Se configura correctamente el modulo de 'i18n' para la plataforma

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:testapp/i18n/i18n.dart';
+import 'package:testapp/services/i18n.dart';
+import 'package:testapp/views/home.dart';
 
-void main() => runApp(Homeflow());
+void main() => runApp(HomeflowApp());
 
-class Homeflow extends StatelessWidget {
+class HomeflowApp extends StatefulWidget {
+  @override
+  _HomeflowAppState createState() => _HomeflowAppState();
+}
 
+class _HomeflowAppState extends State<HomeflowApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      supportedLocales: [
-        Locale('es'),
-        Locale('en')
-      ],
+      onGenerateTitle: (BuildContext context) =>
+          DemoLocalizations.of(context).title,
       localizationsDelegates: [
-        HFLocalizations.delegate,
+        const DemoLocalizationsDelegate(),
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
       ],
-      title: 'Homeflow',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(HFLocalizations.of(context).title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(HFLocalizations.of(context).welcome),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ),
-      ),
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', ''),
+      ],
+      // Watch out: MaterialApp creates a Localizations widget
+      // with the specified delegates. DemoLocalizations.of()
+      // will only find the app's Localizations widget if its
+      // context is a child of the app.
+      home: HomeView(),
     );
   }
 }
---
+++ lib/views/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:testapp/services/i18n.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(DemoLocalizations.of(context).title),
+      ),
+      body: Center(
+        child: Text(DemoLocalizations.of(context).title),
+      ),
+    );
+  }
+}
